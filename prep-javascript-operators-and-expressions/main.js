999297a058ca6f71b9833d03e8847312ea2e2ea7
Kevin <kevbao24@gmail.com>
2023-10-05 22:19:23 +0000

    js-operators-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,44 @@
+const number1 = 1;
+const number2 = 3;
+
+const product = number1 * number2;
+console.log(product);
+
+const charge = 5;
+const payment = 3;
+
+const amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+const tests = 9;
+const assignments = 10;
+const final = 20;
+
+const grade = (tests + assignments + final) / 3;
+console.log(grade);
+console.log(typeof grade);
+
+const firstName = 'Kevin';
+const lastName = 'Bao';
+
+const fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(typeof fullName);
+
+const pH = 8;
+const isAcidic = pH < 7;
+
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+const numberOfSoldiers = 10;
+const isSparta = numberOfSoldiers === 300;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+let nickname = fullName;
+nickname = fullName + ' is the GOAT';
+console.log(nickname);
+console.log(typeof nickname);
